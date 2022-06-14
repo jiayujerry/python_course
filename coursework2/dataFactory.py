297413488f641f83021e8a4c6a06d2fdadd143e6
2021103178刘佳雨 <100659221+jiayujerry@users.noreply.github.com>
2022-06-14 11:04:53 +0800

    Delete coursework2 directory

--- coursework2/dataFactory.py
+++
@@ -1,9 +0,0 @@
-class dataFactory(object):
-    """
-   工厂父类
-    """
-    def __init__(self):
-        self.__name = "dataFactory"
-
-    def sampling(self, **kwargs):
-        raise "This is a base factory, no implement to sample data!"
